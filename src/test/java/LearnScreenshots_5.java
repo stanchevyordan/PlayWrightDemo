2abd18d6551000b373793a229bae128f7f1a5762
Yordan Stanchev <yordan.stanchev1998@gmail.com>
2023-12-04 17:57:30 +0200

    Finished Screenshots lesson

--- src/test/java/LearnScreenshots_5.java
+++ src/test/java/LearnScreenshots_5.java
@@ -2,8 +2,10 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.ScreenshotCaret;
 
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class LearnScreenshots_5 {
 
@@ -27,16 +29,31 @@ public class LearnScreenshots_5 {
 
         // locator screenshot
         Locator bookBtn = page.locator("//button[text()='Book a Demo']");
-
+        bookBtn.screenshot(new Locator.ScreenshotOptions()
+                .setPath(Paths.get("./snaps/locator.png")));
+        Locator header = page.locator(("#header"));
+        header.screenshot(new Locator.ScreenshotOptions().setPath(Paths.get("./snaps/region.png")));
 
         // masking locator
 
+        Locator input = page.locator("input#user-message");
+        input.type("Something");
+        input.scrollIntoViewIfNeeded();
+        page.screenshot(screenshotOptions.setPath(Paths.get("./snaps/input2.png"))
+                .setFullPage(false)                                   // removing the full page option
+                .setMask(Arrays.asList(input)));                      // mask option
 
         // caret show/hide
 
+        input.click();
+        page.screenshot(new Page.ScreenshotOptions().setCaret(ScreenshotCaret.HIDE)
+                .setPath(Paths.get("./snaps/caretHide.png")));                          // without shown typing index
+        page.screenshot(new Page.ScreenshotOptions().setCaret(ScreenshotCaret.INITIAL)       // with shown typing index
+                .setPath(Paths.get("./snaps/caretInitial.png")));
+
 
         page.close();
-        playwright.close();
+        playwright.close();         // screenshots are appearing after we close the page and Playrwright
 
     }
 }
